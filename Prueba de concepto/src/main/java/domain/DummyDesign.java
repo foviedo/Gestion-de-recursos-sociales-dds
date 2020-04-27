1b2a4637a9dab3499380e3a399486d6b69b391cf
Yamila Aylén Soto <yamilaylensoto@gmail.com>
2020-04-27 02:29:39 -0300

    Agrego metodo y test integrante 6

--- Prueba de concepto/src/main/java/domain/DummyDesign.java
+++ Prueba de concepto/src/main/java/domain/DummyDesign.java
@@ -3,18 +3,26 @@ package domain;
 public class DummyDesign {
 
 	public int integrante3() {
-
 		return 3;
 	}
 
 	public int integrante2() {
 		return 2;
 	}
+	
 	public int integrante4() {
 		return 4;
 	}
+	
 	public int integrante5() {
 		return 5;
 	}
-	public int integrante1() { return 1; }
+	
+	public int integrante1() { 
+		return 1;
+	}
+	
+	public int integrante6() {
+		return 6;
+	}
 }
