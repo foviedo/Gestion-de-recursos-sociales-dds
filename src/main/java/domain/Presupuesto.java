70ccd14e0f4fd60680fb5e8ec07c431c0412bc5a
jcastagno99 <Juan-Castagno@Hotmail.com>
2020-06-07 16:50:04 -0300

    Agregamos presupuestos (punto 1 y 2)

--- src/main/java/domain/Presupuesto.java
+++ src/main/java/domain/Presupuesto.java
@@ -1,6 +1,25 @@
 package domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import exception.EgresosException;
+
 public class Presupuesto {
-	double total;
+
+	String detalle;
+	List<Egreso> listaDeEgresos = new ArrayList<Egreso>();
 	
+	public Presupuesto(List<Egreso> listaDeEgresos) { // Obligatoriamente se instancia un presupuesto si tiene previamente egresos
+		if(listaDeEgresos.isEmpty()) {
+			throw new EgresosException();
+		}
+		this.listaDeEgresos = listaDeEgresos;
+	}
+
+	public double total() {
+
+		return listaDeEgresos.stream().mapToDouble(egreso -> egreso.valorTotal()).sum();
+	}
+
 }
---
+++ src/main/java/exception/EgresosException.java
@@ -0,0 +1,8 @@
+package exception;
+
+public class EgresosException extends RuntimeException {
+	public EgresosException() {
+		super("No hay egresos");
+	}
+
+}
