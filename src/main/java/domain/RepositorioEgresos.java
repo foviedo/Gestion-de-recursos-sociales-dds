6085011d5a1515194fa2c3f5e0f585e64ac031fa
Juan Carlos Camacho ruiz <juan.camacho@despegar.com>
2020-07-23 03:04:13 -0300

    fix job

--- src/main/java/domain/RepositorioEgresos.java
+++ src/main/java/domain/RepositorioEgresos.java
@@ -7,24 +7,15 @@ import java.util.stream.Collectors;
 public class RepositorioEgresos {
 	static List<Egreso> todosLosEgresos = new ArrayList<Egreso>();
 	
-	//private static final RepositorioEgresos instance = new RepositorioEgresos();
-
-/*	public static RepositorioEgresos getInstance() {
-		return instance;
-	}
-	*/
-	
 	static List<Egreso> getTodosLosEgresos(){		
 		return RepositorioEgresos.todosLosEgresos;
 	}
 	
 	static List<Egreso> egresosSinValidar(){
-		return todosLosEgresos.stream().filter(unEgreso -> (unEgreso.estoySinValidar())).collect(Collectors.toList());
+		return todosLosEgresos.stream().filter(Egreso::estoySinValidar).collect(Collectors.toList());
 	}
 	static void agregarEgreso(Egreso unEgreso) {
 		todosLosEgresos.add(unEgreso);
 	}
-	
-	
 
 }
--- src/main/java/domain/TareaAEjecutarse.java
+++ src/main/java/domain/TareaAEjecutarse.java
@@ -5,7 +5,7 @@ import java.util.TimerTask;
 public class TareaAEjecutarse extends TimerTask {
 	@Override
 	public void run() {
-		RepositorioEgresos.todosLosEgresos.forEach((unEgreso) -> {unEgreso.validarme();});
+		RepositorioEgresos.egresosSinValidar().forEach(Egreso::validarme);
 		System.out.println("xd esto anda");
 	}
 }
